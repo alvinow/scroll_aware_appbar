62bc887fa2512898647ffefeee270da2af9e60e1
alvinow <alvinowijaksana@gmail.com>
2025-11-15 23:25:26 +0700

    #5 commit

--- lib/scroll_aware_appbar.dart
+++ lib/scroll_aware_appbar.dart
@@ -1,3 +1,3 @@
 library scroll_aware_appbar;
 
-export 'src/scroll_aware_appbar_widget.dart';
+export 'src/scroll_aware_appbar_custom_widget.dart';
---
+++ lib/src/bordered_bottom.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// Wrapper for custom bottom widget with border
+class BorderedBottom extends StatelessWidget implements PreferredSizeWidget {
+  final PreferredSizeWidget bottom;
+  final bool isScrolled;
+  final Color borderColor;
+  final double borderThickness;
+  final Duration animationDuration;
+
+  const BorderedBottom({
+    required this.bottom,
+    required this.isScrolled,
+    required this.borderColor,
+    required this.borderThickness,
+    required this.animationDuration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        bottom,
+        AnimatedContainer(
+          duration: animationDuration,
+          curve: Curves.easeInOut,
+          height: borderThickness,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: isScrolled ? borderColor : Colors.transparent,
+                width: borderThickness,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(
+    bottom.preferredSize.height + borderThickness,
+  );
+}
